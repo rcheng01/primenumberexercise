c18cd4b952217240e158a6a361c45e54518cf8e6
Richard Cheng <richardcheng2001@gmail.com>
2017-12-26 21:46:54 -0600

    added finished exercise

--- prime_number_exercise.py
+++ prime_number_exercise.py
@@ -1,16 +1,29 @@
 # main file
-loop == 1
+print("Hi Jeffrey.")
 
-while loop == 1:
-    print ("Hi Jeffrey.")
+def prime_number_check(x):
+    for i in range(2,x - 1):
+        if x%i == 0:
+            return False
+    return True
 
-    print ("Please input a number.")
-    add1 = input("Do the prime number sum function to this:")
+# return the sum of the primes
+def prime_number_sum(x):
+    primes = []
+    for i in range(2,10 * x + x * x * x):
+        if prime_number_check(i):
+            primes.append(i)
+            a = sum(primes)
+    return a
 
-myint = 10 * add1 + add1 * add1 * add1
 
-def check_prime(x):
-    for i in range(2,)
+
+
+
+
+print(prime_number_sum(1))
+print(prime_number_sum(5))
+print(prime_number_sum(10))
 
 
 
