33194dd5514a22d99032cc7454a329038ff308bb
Richard Cheng <richardcheng2001@gmail.com>
2017-12-26 21:09:33 -0600

    added failed attempt #1

--- prime_number_exercise.py
+++ prime_number_exercise.py
@@ -1,1 +1,20 @@
 # main file
+loop == 1
+
+while loop == 1:
+    print ("Hi Jeffrey.")
+
+    print ("Please input a number.")
+    add1 = input("Do the prime number sum function to this:")
+
+myint = 10 * add1 + add1 * add1 * add1
+
+def check_prime(x):
+    for i in range(2,)
+
+
+
+
+
+
+
